7add81e6a3b61240afe53473bd8f13f881495837
ionut <ionut.barbu198922@gmail.com>
2021-07-30 22:58:38 +0300

    commit 2

--- Tema-1-BarbuIonut.py
+++ Tema-1-BarbuIonut.py
@@ -1,16 +1,16 @@
 my_list=[7,8,9,2,3,1,4,10,5,6]
 print(my_list)
-list_1 = my_list.copy()
-list_1.sort()
-print(list_1)
-list_1.reverse()
-print(list_1)
-my_skiced_list = list_1[::2]
-print(my_skiced_list)
-my_sliced_list2 = list_1[1::2]
-print(my_sliced_list2)
-my_sliced_list3 = list_1[1:8:3]
-print(my_sliced_list3)
+copy_of_list = my_list.copy()
+copy_of_list.sort()
+print(copy_of_list)
+copy_of_list.reverse()
+print(copy_of_list)
+my_sliced_par_list = copy_of_list[::2]
+print(my_sliced_par_list)
+my_sliced_impar_list = copy_of_list[1::2]
+print(my_sliced_impar_list)
+my_sliced_of_number_div_with3_list = copy_of_list[1:8:3]
+print(my_sliced_of_number_div_with3_list)
 
 
 
